311b465c7ebc42f9fa6bea2e0e22f43909540f8d
danicclk <31377550+danicclk@users.noreply.github.com>
2018-02-23 21:12:15 -0600

    Edits and flask app

---
+++ dogs.py
@@ -0,0 +1,11 @@
+
+r = requests.get('https://dog.ceo/api/breeds/list/all')
+r.status_code
+
+r.headers['content-type']
+
+r.encoding
+
+r.text
+
+r.json()
--- venv/jobsexample.py
+++
@@ -1,9 +0,0 @@
-Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> r = requests.get('https://jobs.github.com/positions.json?description=python&location=new+york')
-Traceback (most recent call last):
-  File "<pyshell#0>", line 1, in <module>
-    r = requests.get('https://jobs.github.com/positions.json?description=python&location=new+york')
-NameError: name 'requests' is not defined
->>> import requests
->>> 
