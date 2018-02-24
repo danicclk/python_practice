65c5e7e92f8a4a417004b9b2fa2a906301f8990d
danicclk <31377550+danicclk@users.noreply.github.com>
2018-02-23 23:10:42 -0600

    Edits templates

--- projects/venv/myflaskapp/app.py
+++ projects/venv/myflaskapp/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+# render template that has to be imported
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'INDEX2'
+    return render_template('home.html')        # pass your:render template
 
 
 if __name__ == '__main__':
