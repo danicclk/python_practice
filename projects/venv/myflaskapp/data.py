77a59a19c532e891d131e416fa6efe5f36f78bb6
danicclk <31377550+danicclk@users.noreply.github.com>
2018-02-24 11:08:27 -0600

    Edits, routes for about, articles
    Added new routes.
    HTML templates:  about, articles

---
+++ projects/venv/myflaskapp/data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'This is an article about Python Programming',
+            'author': 'Daniela Clark',
+            'create_date': '02-23-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'This is an article about Python Programming.',
+            'author': 'Author 2',
+            'create_date': '02-23-2018',
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'This is an article about Python Programming.',
+            'author': 'Author 3',
+            'create_date': '02-23-2018'
+        }
+
+    ]
+    return articles
