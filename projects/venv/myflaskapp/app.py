f8d09dc44d341945d194a25dea990ad03da39327
danicclk <31377550+danicclk@users.noreply.github.com>
2018-03-04 09:25:55 -0600

    mysql edits

--- projects/venv/myflaskapp/app.py
+++ projects/venv/myflaskapp/app.py
@@ -1,15 +1,45 @@
 # render template that has to be imported
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+
 
 app = Flask(__name__)
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MYSQL
+mysql = MySQL(app)
 
 Articles = Articles()
 
 
 @app.route('/')
 def index():
-    return render_template('home.html')        # pass your:render template
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get users
+    result = cur.execute("SELECT * FROM users")
+
+    users = cur.fetchall()
+    print(users)
+
+    if result > 0:
+        return render_template('home.html', users=users)
+
+    else:
+        msg = 'No Users Found'
+        return render_template('home.html', msg=msg)
+    # Close connection
+    cur.close()
 
 
 @app.route('/about')
