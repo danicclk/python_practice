41971bd761424164ba191dbc91cceaf72672b97b
danicclk <31377550+danicclk@users.noreply.github.com>
2018-03-08 12:41:40 -0600

    Form edits
    Addressing the field errors.

--- projects/venv/myflaskapp/app.py
+++ projects/venv/myflaskapp/app.py
@@ -70,13 +70,35 @@ class RegisterForm(Form):
     confirm = PasswordField('Confirm Password')
 
 
+# User Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('register.html')
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",
+                    (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('index'))
     return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'secret123'
     app.run(debug=True)
