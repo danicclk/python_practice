9ad0b3c7ecbe6b958165d0a9ae196d43b30c8433
danicclk <31377550+danicclk@users.noreply.github.com>
2018-03-11 10:57:03 -0500

    Edits messages, navbar, register
    Re-direct register to login
    Added Register bottom on top-right

--- projects/venv/myflaskapp/app.py
+++ projects/venv/myflaskapp/app.py
@@ -95,7 +95,7 @@ def register():
 
         flash('You are now registered and can log in', 'success')
 
-        return redirect(url_for('index'))
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 
