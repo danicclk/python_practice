92fe4208ddbea5fe8b4bffd969ecf2bb3ac63d73
danicclk <31377550+danicclk@users.noreply.github.com>
2018-03-27 01:44:29 -0500

    edits

--- scrapy_fun/tutorial/tutorial/spiders/quotes_spiders.py
+++ scrapy_fun/tutorial/tutorial/spiders/quotes_spiders.py
@@ -5,15 +5,17 @@ import scrapy
 class QuotesSpider(scrapy.Spider):  # define a class
     name = "quotes"  # define the variable, identifies the Spider. Must be unique.
     allowed_domains = ["toscrape.com"]
-    start_urls = ['http://quotes.toscrape.com/random']
+    start_urls = ["http://quotes.toscrape.com"]
   
-   #call back method
+   # call back method
     def parse(self, response):
         self.log('I just visited:' + response.url)
-        # dictionary 
-        yield {
+        for quote in response.css('div.quote'):
+            items= {
             'author_name': response.css('small.author::text').extract_first(),
             'text': response.css('span.text::text').extract_first(),
             'tage': response.css('a.tag::text').extract(),
-        }
+            }
+            yield item 
+        # follow pagination link
         
