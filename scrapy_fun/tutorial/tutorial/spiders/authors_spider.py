643d97561ac6faa232945dc9ecbe7ac4eaa67c4a
danicclk <31377550+danicclk@users.noreply.github.com>
2018-03-28 01:18:34 -0500

    authors spider

---
+++ scrapy_fun/tutorial/tutorial/spiders/authors_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+
+class AuthorsSpider(scrapy.Spider):  # define a class
+    name = "authors"  # define the variable, identifies the Spider. Must be unique.
+    allowed_domains = ["toscrape.com"]
+    start_urls = ["http://quotes.toscrape.com"]
+  
+   # call back method
+    def parse(self, response):
+        urls = response.css('div.quote > span > a::attr(href)').extract()
+        for url in urls:
+            url = response.urljoin(url)
+            yield scrapy.Request(url=url, callback=self.parse_details)
+        # follow pagination link
+            # this line just gets the relative url
+        next_page_url = response.css('li.next > a::attr(href)').extract_first()
+        if next_page_url: # spider will stop once there're no more pages
+            next_page_url = response.urljoin(next_page_url)
+            # the spiderneeds to generate new request
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
+
+    def parse_details(self, response):
+        pass 
