c99b2774d40802848a721918fa737a0f41b2c421
danicclk <31377550+danicclk@users.noreply.github.com>
2018-05-07 07:57:50 -0500

    Practice

--- scrapy_practice/scrapy_fun/tutorial/tutorial/spiders/quotes_spider.py
+++ scrapy_practice/scrapy_fun/tutorial/tutorial/spiders/quotes_spider.py
@@ -5,31 +5,24 @@ from scrapy_splash import SplashRequest
 class QuotesJSSpider(scrapy.Spider):
     name = 'quotejs'
 
-    start_urls = ['https://illinoisjoblink.illinois.gov/ada/r/search/jobs?keywords=Developer&location=Chicago&radius=50']
-
     def start_requests(self):
-        for url in self.start_urls:
-            yield SplashRequest(url, self.parse, args={'wait': 2})
+        yield SplashRequest(
+            url='',
+            callback=self.parse,
+        )
 
     def parse(self, response):
+        for quote in response.css("div.quote"):
+            # css selectors below
+            yield {
+            }
+
         # follow links to job description page
-        for href in response.css('.clickcard .jobtitle::attr(href)'):
-            yield response.follow(href, self.parse_jobs)
 
-        # follow pagination links
-        for href in response.css('.pagination a:last-child::attr(href)'):
-            yield response.follow(href, self.parse)
 
-    def parse_jobs(self, response):
-        def extract_with_css(query):
-            if response.css(query).extract_first():
-                return response.css(query).extract_first().strip()
-            else:
-                return ''
-
-        yield {
-            # 'job_title': extract_with_css('.jobtitle::text'),
-            # 'company': extract_with_css('.company::text'),
-            # 'snip': extract_with_css('.snip::text'),
-            'page_title': extract_with_css('title::text')
-        }
+        # yield {
+        #     # 'job_title': extract_with_css('.jobtitle::text'),
+        #     # 'company': extract_with_css('.company::text'),
+        #     # 'snip': extract_with_css('.snip::text'),
+        #     'page_title': extract_with_css('title::text')
+        # }
